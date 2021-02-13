7ece95645491e8bdd74538394f14e8b49918bc9b
Sandro Skhirtladze <s.skhirtladze@altasoft.ge>
2021-02-14 01:39:02 +0400

    SQL QUERY EXECUTOR ADDED

---
+++ SQLTester/SQLTester/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿
+namespace SQLTester
+{
+    partial class frmTester
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grdRecords = new System.Windows.Forms.DataGridView();
+            this.txtCommand = new System.Windows.Forms.TextBox();
+            this.btnExecute = new System.Windows.Forms.Button();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.grdRecords)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grdRecords
+            // 
+            this.grdRecords.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdRecords.Location = new System.Drawing.Point(12, 12);
+            this.grdRecords.Name = "grdRecords";
+            this.grdRecords.Size = new System.Drawing.Size(776, 304);
+            this.grdRecords.TabIndex = 0;
+            // 
+            // txtCommand
+            // 
+            this.txtCommand.Location = new System.Drawing.Point(12, 332);
+            this.txtCommand.Multiline = true;
+            this.txtCommand.Name = "txtCommand";
+            this.txtCommand.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtCommand.Size = new System.Drawing.Size(626, 106);
+            this.txtCommand.TabIndex = 1;
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExecute.Location = new System.Drawing.Point(644, 325);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(144, 33);
+            this.btnExecute.TabIndex = 2;
+            this.btnExecute.Text = "Execute Statement";
+            this.btnExecute.UseVisualStyleBackColor = true;
+            this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCount.Location = new System.Drawing.Point(753, 368);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(16, 16);
+            this.lblCount.TabIndex = 3;
+            this.lblCount.Text = "0";
+            this.lblCount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(674, 368);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Record Count";
+            // 
+            // frmTester
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.btnExecute);
+            this.Controls.Add(this.txtCommand);
+            this.Controls.Add(this.grdRecords);
+            this.Name = "frmTester";
+            this.Text = "SQL Tester";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmTester_Closing);
+            this.Load += new System.EventHandler(this.frmTester_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grdRecords)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView grdRecords;
+        private System.Windows.Forms.TextBox txtCommand;
+        private System.Windows.Forms.Button btnExecute;
+        private System.Windows.Forms.Label lblCount;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ SQLTester/SQLTester/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQLTester
+{
+    public partial class frmTester : Form
+    {
+        private OleDbConnection conn;
+        public frmTester()
+        {
+            InitializeComponent();
+        }
+
+        private void frmTester_Load(object sender, EventArgs e)
+        {
+            string dataPath = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.Parent.FullName +
+                              "\\DB\\Books.accdb";
+            var connString = $"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={dataPath};Persist Security Info=False;";
+            conn = new OleDbConnection(connString);
+            conn.Open();
+        }
+
+        private void btnExecute_Click(object sender, EventArgs e)
+        {
+            OleDbCommand command = null;
+            OleDbDataAdapter adapter = new OleDbDataAdapter();
+            DataTable table = new DataTable();
+
+            try
+            {
+                command = new OleDbCommand(txtCommand.Text,conn);
+                adapter.SelectCommand = command;
+                adapter.Fill(table);
+
+                grdRecords.DataSource = table;
+
+                lblCount.Text = table.Rows.Count.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            command.Dispose();
+            adapter.Dispose();
+            table.Dispose();
+        }
+
+        private void frmTester_Closing(object sender, FormClosingEventArgs e)
+        {
+            conn.Close();
+            conn.Dispose();
+        }
+    }
+}
