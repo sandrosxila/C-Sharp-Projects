ea83d97b2be67b6327878ee8bf1bdb3927f4c839
Sandro Skhirtladze <s.skhirtladze@altasoft.ge>
2021-02-14 02:55:42 +0400

    Book Database Searcher Added

---
+++ BookDatabaseSearchProject/BookDatabaseSearchProject/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace BookDatabaseSearchProject
+{
+    partial class frmDatabase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grdBooks = new System.Windows.Forms.DataGridView();
+            this.btnAllRecords = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.grdBooks)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grdBooks
+            // 
+            this.grdBooks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdBooks.Location = new System.Drawing.Point(11, 15);
+            this.grdBooks.Name = "grdBooks";
+            this.grdBooks.Size = new System.Drawing.Size(777, 275);
+            this.grdBooks.TabIndex = 0;
+            // 
+            // btnAllRecords
+            // 
+            this.btnAllRecords.Location = new System.Drawing.Point(651, 415);
+            this.btnAllRecords.Name = "btnAllRecords";
+            this.btnAllRecords.Size = new System.Drawing.Size(137, 23);
+            this.btnAllRecords.TabIndex = 1;
+            this.btnAllRecords.Text = "Show All Records";
+            this.btnAllRecords.UseVisualStyleBackColor = true;
+            this.btnAllRecords.Click += new System.EventHandler(this.btnSQL_Click);
+            // 
+            // frmDatabase
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAllRecords);
+            this.Controls.Add(this.grdBooks);
+            this.Name = "frmDatabase";
+            this.Text = "Database";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmForm_Closing);
+            this.Load += new System.EventHandler(this.frmDatabase_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grdBooks)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView grdBooks;
+        private System.Windows.Forms.Button btnAllRecords;
+    }
+}
+
---
+++ BookDatabaseSearchProject/BookDatabaseSearchProject/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BookDatabaseSearchProject
+{
+    public partial class frmDatabase : Form
+    {
+        Button[] btnButtons = new Button[26];
+        private OleDbConnection conn;
+
+        private string sql = "SELECT a.Author, t.Title, p.Name " +
+                             "FROM Authors as a, Titles as t, Publishers as p, Title_Author as ta " +
+                             "WHERE a.AU_ID = ta.AU_ID " +
+                             "AND t.ISBN = ta.ISBN " +
+                             "AND t.pubID = p.pubID";
+        public frmDatabase()
+        {
+            InitializeComponent();
+        }
+
+        private void frmDatabase_Load(object sender, EventArgs e)
+        {
+            int btnWidth, btnLeft, btnTop;
+            int btnHeight = 30;
+
+
+            string dataPath = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.Parent.FullName +
+                              "\\DB\\Books.accdb";
+            var connString = $"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={dataPath};Persist Security Info=False;";
+            conn = new OleDbConnection(connString);
+            conn.Open();
+
+            btnWidth = ClientSize.Width / 13;
+            btnLeft = ClientSize.Width - (13 * btnWidth);
+            btnTop = grdBooks.Top + grdBooks.Height + 15;
+
+            for (int i = 0; i < 26; i++)
+            {
+                btnButtons[i] = new Button();
+                btnButtons[i].Text = ((char)(65 + i)).ToString();
+                btnButtons[i].Width = btnWidth;
+                btnButtons[i].Height = btnHeight;
+                btnButtons[i].Left = btnLeft + (btnWidth * (i % 13));
+                btnButtons[i].Top = btnTop;
+                if (i > 12) btnButtons[i].Top += btnHeight + 5;
+                Controls.Add(btnButtons[i]);
+                btnButtons[i].Click += new EventHandler(btnSQL_Click);
+            }
+
+        }
+
+        private void btnSQL_Click(object sender, EventArgs e)
+        {
+            OleDbCommand command = null;
+            OleDbDataAdapter adapter = new OleDbDataAdapter();
+            DataTable table = new DataTable();
+            Button clickedButton = (Button) sender;
+            string sqlStatement;
+            switch (clickedButton.Text)
+            {
+                case "Show All Records":
+                    sqlStatement = sql;
+                    break;
+                default:
+                    sqlStatement = $"{sql} AND a.Author LIKE '{clickedButton.Text}%'";
+                    break;
+            }
+
+            try
+            {
+                command = new OleDbCommand(sqlStatement,conn);
+                adapter.SelectCommand = command;
+                adapter.Fill(table);
+                grdBooks.DataSource = table;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error in SQL", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            adapter.Dispose();
+            table.Dispose();
+        }
+
+        private void frmForm_Closing(object sender, FormClosingEventArgs e)
+        {
+            conn.Close();
+            conn.Dispose();
+        }
+    }
+}
