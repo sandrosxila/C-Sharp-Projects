5a4539fa9cdac9f24ba54f57cff8c2fe3983dce8
sandrosxila <sandrosxila@yahoo.com>
2021-03-01 18:11:22 +0400

    Asynchronous Form

---
+++ ParallelProgramming/AsyncAwaitPractice/AsyncAwaitPractice.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace AsyncAwaitPractice
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClickMe = new System.Windows.Forms.Button();
+            this.lblText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnClickMe
+            // 
+            this.btnClickMe.Location = new System.Drawing.Point(12, 83);
+            this.btnClickMe.Name = "btnClickMe";
+            this.btnClickMe.Size = new System.Drawing.Size(160, 23);
+            this.btnClickMe.TabIndex = 0;
+            this.btnClickMe.Text = "Click Me";
+            this.btnClickMe.UseVisualStyleBackColor = true;
+            this.btnClickMe.Click += new System.EventHandler(this.btnClickMe_Click);
+            // 
+            // lblText
+            // 
+            this.lblText.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.lblText.Location = new System.Drawing.Point(12, 27);
+            this.lblText.Name = "lblText";
+            this.lblText.Size = new System.Drawing.Size(160, 28);
+            this.lblText.TabIndex = 1;
+            this.lblText.Text = "Click To Update (5s)";
+            this.lblText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(184, 118);
+            this.Controls.Add(this.lblText);
+            this.Controls.Add(this.btnClickMe);
+            this.Name = "Form1";
+            this.Text = "AsyncAwaitPractice";
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Label lblText;
+
+        private System.Windows.Forms.Button btnClickMe;
+
+        #endregion
+    }
+}
---
+++ ParallelProgramming/AsyncAwaitPractice/AsyncAwaitPractice.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsyncAwaitPractice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static async Task<string> UpdateText()
+        {
+            await Task.Delay(5000);
+            return "Updated";
+        }
+        
+        private async void btnClickMe_Click(object sender, EventArgs e)
+        {
+            var updateTask = await UpdateText();
+            lblText.Text = updateTask;
+
+            await Task.Delay(5000);
+
+            using (var wc = new WebClient())
+            {
+                string data =
+                    await wc.DownloadStringTaskAsync("http://google.com/robots.txt");
+                lblText.Text = data.Split("\n")[0].Trim();
+            }
+        }
+    }
+}
