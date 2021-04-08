8e14fe15bd2bf0dbdced7e33f011e57477c85fae
Sandro Skhirtladze <s.skhirtladze@altasoft.ge>
2021-04-08 15:35:58 +0400

    Query Beautifier

---
+++ QueryBeautifier/QueryBeautifier/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace QueryBeautifier
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtLeft = new System.Windows.Forms.RichTextBox();
+            this.txtRight = new System.Windows.Forms.RichTextBox();
+            this.btnFormat = new System.Windows.Forms.Button();
+            this.chkMatches = new System.Windows.Forms.CheckBox();
+            this.txtDiff = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chkSort = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // txtLeft
+            // 
+            this.txtLeft.Font = new System.Drawing.Font("Calibri", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLeft.Location = new System.Drawing.Point(16, 44);
+            this.txtLeft.Margin = new System.Windows.Forms.Padding(4);
+            this.txtLeft.Name = "txtLeft";
+            this.txtLeft.Size = new System.Drawing.Size(294, 616);
+            this.txtLeft.TabIndex = 0;
+            this.txtLeft.Text = "";
+            // 
+            // txtRight
+            // 
+            this.txtRight.Font = new System.Drawing.Font("Calibri", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRight.Location = new System.Drawing.Point(318, 44);
+            this.txtRight.Margin = new System.Windows.Forms.Padding(4);
+            this.txtRight.Name = "txtRight";
+            this.txtRight.Size = new System.Drawing.Size(294, 616);
+            this.txtRight.TabIndex = 1;
+            this.txtRight.Text = "";
+            this.txtRight.TextChanged += new System.EventHandler(this.txtRight_Changed);
+            // 
+            // btnFormat
+            // 
+            this.btnFormat.Location = new System.Drawing.Point(195, 3);
+            this.btnFormat.Margin = new System.Windows.Forms.Padding(4);
+            this.btnFormat.Name = "btnFormat";
+            this.btnFormat.Size = new System.Drawing.Size(245, 36);
+            this.btnFormat.TabIndex = 2;
+            this.btnFormat.Text = "Format";
+            this.btnFormat.UseVisualStyleBackColor = true;
+            this.btnFormat.Click += new System.EventHandler(this.btnFormat_Click);
+            // 
+            // chkMatches
+            // 
+            this.chkMatches.AutoSize = true;
+            this.chkMatches.Checked = true;
+            this.chkMatches.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkMatches.Location = new System.Drawing.Point(466, 19);
+            this.chkMatches.Name = "chkMatches";
+            this.chkMatches.Size = new System.Drawing.Size(119, 20);
+            this.chkMatches.TabIndex = 3;
+            this.chkMatches.Text = "Check Matches";
+            this.chkMatches.UseVisualStyleBackColor = true;
+            // 
+            // txtDiff
+            // 
+            this.txtDiff.Font = new System.Drawing.Font("Calibri", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDiff.Location = new System.Drawing.Point(620, 44);
+            this.txtDiff.Margin = new System.Windows.Forms.Padding(4);
+            this.txtDiff.Name = "txtDiff";
+            this.txtDiff.Size = new System.Drawing.Size(483, 616);
+            this.txtDiff.TabIndex = 1;
+            this.txtDiff.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(617, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Difference:";
+            // 
+            // chkSort
+            // 
+            this.chkSort.AutoSize = true;
+            this.chkSort.Checked = true;
+            this.chkSort.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkSort.Location = new System.Drawing.Point(466, 3);
+            this.chkSort.Name = "chkSort";
+            this.chkSort.Size = new System.Drawing.Size(51, 20);
+            this.chkSort.TabIndex = 3;
+            this.chkSort.Text = "Sort";
+            this.chkSort.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1116, 668);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chkSort);
+            this.Controls.Add(this.chkMatches);
+            this.Controls.Add(this.btnFormat);
+            this.Controls.Add(this.txtDiff);
+            this.Controls.Add(this.txtRight);
+            this.Controls.Add(this.txtLeft);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Query String Formatter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtLeft;
+        private System.Windows.Forms.RichTextBox txtRight;
+        private System.Windows.Forms.Button btnFormat;
+        private System.Windows.Forms.CheckBox chkMatches;
+        private System.Windows.Forms.RichTextBox txtDiff;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox chkSort;
+    }
+}
+
---
+++ QueryBeautifier/QueryBeautifier/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QueryBeautifier
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFormat_Click(object sender, EventArgs e)
+        {
+            txtDiff.Text = "";
+            txtRight.ForeColor = Color.Black;
+            txtLeft.Text = string.Join(" = ",string.Join("\n", txtLeft.Text.Split('&')).Replace(" ","").Split('='));
+            txtRight.Text = string.Join(" = ", string.Join("\n", txtRight.Text.Split('&')).Replace(" ", "").Split('='));
+
+            if (chkSort.Checked)
+            {
+                txtLeft.Text = string.Join("\n", txtLeft.Text.Split('\n').OrderBy(x => x).ToArray());
+                txtRight.Text = string.Join("\n", txtRight.Text.Split('\n').OrderBy(x => x).ToArray());
+            }
+
+            Dictionary<string, string> leftDict = new Dictionary<string, string>();
+            Dictionary<string, string> rightDict = new Dictionary<string, string>();
+
+            foreach (string pair in txtLeft.Text.Split('\n'))
+            {
+                var arr = pair.Split('=');
+                if(arr.Length == 2)
+                    leftDict[arr[0]] = arr[1];
+            }
+
+            foreach (string pair in txtRight.Text.Split('\n'))
+            {
+                var arr = pair.Split('=');
+                if (arr.Length == 2)
+                    rightDict[arr[0]] = arr[1];
+            }
+
+            if (chkMatches.Checked)
+            {
+                if (txtLeft.Text == txtRight.Text)
+                    txtRight.ForeColor = Color.Green;
+                else
+                {
+                    List<string> leftList = new List<string>(leftDict.Keys);
+                    List<string> rightList = new List<string>(rightDict.Keys);
+
+                    List<string> intersectionList = leftList.AsQueryable().Intersect(rightList).ToList();
+                    List<string> nonIntersectionList = leftList.Except(rightList).Union(rightList.Except(leftList)).ToList();
+
+                    int matchCount = intersectionList.Select(k => leftDict[k] != rightDict[k]).Count();
+                    if (matchCount > 0)
+                    {
+                        txtDiff.Text += "Matches:\n";
+                    }
+
+                    foreach(string key in intersectionList)
+                    {
+                        var x = leftDict[key];
+                        var y = rightDict[key];
+                        if (leftDict[key] != rightDict[key])
+                        {
+                            txtDiff.Text += $"{key} -> ";
+                            txtDiff.Text += $"Left: {leftDict[key]} \t Right: {rightDict[key]}\n";
+                        }
+                    }
+
+                    if(matchCount>0)
+                        txtDiff.Text += "\n";
+
+                    if (nonIntersectionList.Count > 0)
+                    {
+                        txtDiff.Text += "Not Match:\n";
+                    }
+
+                    foreach(string key in nonIntersectionList)
+                    {
+                        txtDiff.Text += $"{key} -> ";
+                        if(leftList.Contains(key))
+                            txtDiff.Text += $"Left: {leftDict[key]}\n";
+                        if(rightList.Contains(key))
+                            txtDiff.Text += $"Right: {rightDict[key]}\n";
+                    }
+                }
+            }
+        }
+
+        private void txtRight_Changed(object sender, EventArgs e)
+        {
+            if(txtRight.ForeColor == Color.Green)
+                txtRight.ForeColor = Color.Black;
+        }
+    }
+}
